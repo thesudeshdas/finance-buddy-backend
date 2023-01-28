5137937602b7dc309aca2a7ff342827d8209b1cc
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-28 19:59:57 +0530

    feat: add transaction

--- controllers/transactions.controller.ts
+++ controllers/transactions.controller.ts
@@ -16,3 +16,24 @@ exports.get_transactions_get = async (req: Request, res: Response) => {
     console.error('yahan aaya', error);
   }
 };
+
+// add a transaction
+exports.add_transaction_post = async (req: Request, res: Response) => {
+  try {
+    const name = req.body.name;
+    const color = req.body.color;
+
+    const response = await pool.query(
+      `INSERT INTO shark (name, color) VALUES ($1, $2) RETURNING *`,
+      [name, color]
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: 'Successfully fetched shark table',
+      addedTransaction: response.rows[0],
+    });
+  } catch (error) {
+    console.error('ab yahan pe', error);
+  }
+};
--- routes/transactions.route.ts
+++ routes/transactions.route.ts
@@ -1,28 +1,10 @@
 import express from 'express';
 const router = express.Router();
 
-import { pool } from '../db';
-
 const transactions_controller = require('../controllers/transactions.controller');
 
 router.get('/', transactions_controller.get_transactions_get);
 
-router.post('/', async (req, res) => {
-  try {
-    console.log(req.body);
-
-    const name = req.body.name;
-    const color = req.body.color;
-
-    const response = await pool.query(
-      `INSERT INTO shark (name, color) VALUES ($1, $2)`,
-      [name, color]
-    );
-
-    console.log({ response });
-  } catch (error) {
-    console.log({ error });
-  }
-});
+router.post('/', transactions_controller.add_transaction_post);
 
 module.exports = router;
