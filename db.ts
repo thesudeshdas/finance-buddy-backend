5c2b62cd0106176de5278bcaf2bea105fd464144
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-28 23:09:06 +0530

    feat: connect to hosted DB

--- db.ts
+++ db.ts
@@ -1,9 +1,14 @@
 const { Pool } = require('pg');
 
+// export const pool = new Pool({
+//   user: 'dash',
+//   database: 'ocean_fgmi?ssl=true',
+//   password: 'L89QpTqb71laa0WTr6423oVHtGKwSxcl',
+//   port: 5432,
+//   host: 'dpg-cfakcahgp3jsh6fisang-a.oregon-postgres.render.com',
+// });
+
 export const pool = new Pool({
-  user: 'dash',
-  database: 'fish',
-  password: 'password',
-  port: 5432,
-  host: 'localhost',
+  connectionString:
+    'postgres://dash:L89QpTqb71laa0WTr6423oVHtGKwSxcl@dpg-cfakcahgp3jsh6fisang-a.oregon-postgres.render.com/ocean_fgmi?ssl=true',
 });
